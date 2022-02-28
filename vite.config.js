4d59c3abc48f74cfc71e9dc2621f7f0f4c04ac5c
hsinpaohuang <hsinpaohuang@gmail.com>
2022-02-28 18:51:15 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    eslintPlugin(),
+    vue(),
+  ],
+  server: {
+    port: 8100,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
+})
